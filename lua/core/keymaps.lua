cae6200534efe9137f39ad1507acb50867fd6e4b
kbeerta <koenbeerta@gmail.com>
2025-01-07 19:43:00 +0100

    last one

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,7 @@
 
 local map = function(mode, lhs, rhs, opts)
   local opts = vim.tbl_extend("force", opts or {}, { noremap = true, silent = true })
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+  vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
 end
 
 map("n", "<S-h>", "<cmd>bprev<CR>")
