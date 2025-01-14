0d6e8f8d00f2bf9719f64be19587372c11d68a4f
kbeerta <koenbeerta@gmail.com>
2025-01-14 18:28:57 +0100

    live_grep

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -9,7 +9,7 @@ map("n", "<S-l>", "<cmd>bnext<CR>")
 
 map("n", "-", "<cmd>Oil<CR>")
 
-map("n", "<leader>fg", "<cmd>FzfLua grep<CR>")
 map("n", "<leader>ff", "<cmd>FzfLua files<CR>")
 map("n", "<leader>fb", "<cmd>FzfLua buffers<CR>")
+map("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>")
 
