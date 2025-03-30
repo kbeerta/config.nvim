10084d93ec31be996901b55f2ef344aa6afd5df4
kbeerta <koenbeerta@gmail.com>
2025-03-30 10:26:49 +0200

    add more Snacks pickers

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -11,9 +11,14 @@ return {
         },
     },
     keys = {
+        -- find
+        { "<leader>ff", function() Snacks.picker.files() end },
         { "<leader>ff", function() Snacks.picker.files() end },
         { "<leader>fb", function() Snacks.picker.buffers() end },
+        { "<leader>fr", function() Snacks.picker.recent() end },
 
+        -- search
         { "<leader>sg", function() Snacks.picker.grep() end },
+        { "<leader>sj", function() Snacks.picker.jumps() end },
     }
 }
